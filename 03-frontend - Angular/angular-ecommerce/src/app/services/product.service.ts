d795d175bdb950863156c36c2033d2144212e2de
ANANT KUMAR JAIN <anantkr.developer@gmail.com>
2020-08-26 18:17:57 +0530

    Angular development for Release 2.0 - Search for products by category, keyword and master-detail view

--- 03-frontend - Angular/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend - Angular/angular-ecommerce/src/app/services/product.service.ts
@@ -3,25 +3,64 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products?size=100';
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProduct(theProductId: number): Observable<Product> {
+
+    //Need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
+
+  getProductList(theCategoryId: number): Observable<Product[]>{
+
+    // Need to build URL based on category id.
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}&size=100`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+
+     // Need to build URL based on keyword.
+     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}&size=100`;
+
+     return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse{
+interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
