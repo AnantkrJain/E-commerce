786cc26fcc2a04d0a589a55cb8ca739e46a944ba
ANANT KUMAR JAIN <anantkr.developer@gmail.com>
2020-08-27 07:54:04 +0530

    Angular: Pagination using ng-bootstrap- basic, adding boundaries, selecting page size

--- 03-frontend - Angular/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend - Angular/angular-ecommerce/src/app/services/product.service.ts
@@ -24,6 +24,17 @@ export class ProductService {
     return this.httpClient.get<Product>(productUrl);
   }
 
+  getProductListPaginate(thePage: number,
+                         thePageSize: number,
+                         theCategoryId: number): Observable<GetResponseProducts>{
+
+    // Need to build URL based on category id, page and size.
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                       + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]>{
 
     // Need to build URL based on category id.
@@ -38,6 +49,17 @@ export class ProductService {
      const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}&size=100`;
 
      return this.getProducts(searchUrl);
+  }
+
+  searchProductsPaginate(thePage: number,
+                         thePageSize: number,
+                         theKeyword: string): Observable<GetResponseProducts>{
+
+  // Need to build URL based on keyword, page and size.
+  const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                     + `&page=${thePage}&size=${thePageSize}`;
+
+  return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   private getProducts(searchUrl: string): Observable<Product[]> {
@@ -56,6 +78,12 @@ export class ProductService {
 interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
